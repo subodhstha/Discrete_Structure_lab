a0c4735d2f2f41ab362064660823f63e82b85995
subodhstha <shresthasubodh73@gmail.com>
2022-07-02 15:47:45 +0545

    ds

---
+++ 2_ceiling_floor.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    printf("Floor of 2.2 is %f\n",floor(2.2));
+    printf("Floor of -2.2 is %f\n",floor(-2.2));
+    printf("Floor of 5.8 is %f\n",floor(5.8));
+    printf("Celling of 2.2 is%f\n",ceil(2.2));
+    printf("Celling of -2.2 is %f\n",ceil(-2.2));
+    printf("Celling of 5.8 is%f\n",ceil(5.8));
+
+}
---
+++ 3_eulidian_algorithm.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int gcd(int a, int b)
+{
+    if (a == 0)
+    {
+        return b;
+    }
+    return gcd(b % a, a);
+}
+int main()
+{
+    int a, b;
+    printf("Enter any two number\n");
+    scanf("%d %d",&a ,&b);
+    printf("The GCD of %d & %d is %d",a ,b ,gcd(a, b));
+    return 0;
+}
